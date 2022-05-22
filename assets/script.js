e903b58e811b660c3deb2d0b8bdff6ffd02142ff
Leandro Michelena <leandromichelena@gmail.com>
2022-05-22 01:00:39 -0400

    Added function to return 5 days forecast. Updated various page elements

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ var citySearch = document.querySelector("#citySearchForm");
 var cityInputEl = document.querySelector("#cityNameSearch");
 var cityNameEl = document.querySelector("#cityNameDisplay");
 var currentWeatherParagraph = document.querySelector("#currentWeatherParagraph")
+var forecastCardsEl = document.querySelector("#forecastCards");
 
 var formSubmitHandler = function (event) {
     // prevents browser from refreshing when clicking the submit button
@@ -50,7 +51,11 @@ var getWeatherInfo = function(cityName) {
     var state = cityName[0].state
     var name = cityName[0].name
 
-    cityNameEl.innerHTML = name + ", " + state + ", " + country
+    cityNameEl.innerHTML = name + "<br>" + state + ", " + country
+    
+    if (state === undefined){ 
+        cityNameEl.innerHTML = name + "<br>" + country
+    }
 
     var apiWeather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=metric&appid=" + APIKey
 
@@ -67,17 +72,48 @@ var getWeatherInfo = function(cityName) {
 };
 
 var displayWeatherInfo = function(WeatherInfo) {
-    currentWeatherIcon = WeatherInfo.current.weather[0].icon
-    iconSource0 = "http://openweathermap.org/img/wn/" + currentWeatherIcon + ".png"
-    console.log(iconSource0)
-    var infoSpan = '<img src=' + iconSource0 + '>';
-    var temp = WeatherInfo.current.temp + " °C";
-    var wind = WeatherInfo.current.wind_speed + " meter/sec";
-    var humidity = WeatherInfo.current.humidity + "%";
-    var UVIndex = WeatherInfo.current.uvi;
+    var currentWeatherIcon = WeatherInfo.current.weather[0].icon
+    var iconSource0 = "http://openweathermap.org/img/wn/" + currentWeatherIcon + ".png"
+
+    var currentDescription = WeatherInfo.current.weather[0].description.toUpperCase();
+    var currentDate = dayjs.unix(WeatherInfo.current.dt)
+    var infoSpan = ' - ' + currentDate.format('DD/MM/YYYY') + ' <img src=' + iconSource0 + '>';
+    var temp = "Temp: " + WeatherInfo.current.temp + " °C";
+    var wind = "Wind: " + WeatherInfo.current.wind_speed + " m/s";
+    var humidity = "Humidity: " + WeatherInfo.current.humidity + "%";
+    var UVIndex = "UV Index: " + '<span id="UV-index">' + WeatherInfo.current.uvi + '</span>';
 
     cityNameEl.innerHTML = cityNameEl.innerHTML + infoSpan
-    currentWeatherParagraph.innerHTML = temp + '<br>' + wind + '<br>' + humidity + '<br>' + UVIndex;
+    currentWeatherParagraph.innerHTML = currentDescription + '<br>' + temp + '<br>' + wind + '<br>' + humidity + '<br>' + UVIndex;
+
+    var uvSpan = document.querySelector("#UV-index");
+    uvSpan.style.padding = "2px 8px";
+    if (WeatherInfo.current.uvi <= 2) {
+        uvSpan.style.backgroundColor = "green";
+        uvSpan.style.color = "white";
+    } else if (WeatherInfo.current.uvi > 2 && WeatherInfo.current.uvi <= 5) {
+        uvSpan.style.backgroundColor = "yellow";
+        uvSpan.style.color = "black";
+    } else if (WeatherInfo.current.uvi > 5 && WeatherInfo.current.uvi <= 7){
+        uvSpan.style.backgroundColor = "orange";
+        uvSpan.style.color = "white";
+    } else {
+        uvSpan.style.backgroundColor = "red";
+        uvSpan.style.color = "white";
+    }
+
+    forecastCardsEl.innerHTML = "<h3 class='pt-3'> Five days forecast:</h3>"
+    for (i = 1; i < 6; i++) {
+        var dateDaily = '<strong>' + dayjs.unix(WeatherInfo.daily[i].dt).format('DD/MM/YYYY') + '</strong>';
+        var iconSourceDaily = WeatherInfo.daily[i].weather[0].icon;
+        var iconDaily = '<img src="http://openweathermap.org/img/wn/' + iconSourceDaily + '.png">'
+        var tempDaily = "Temp: " + WeatherInfo.daily[i].temp.day + " °C";
+        var windDaily = "Wind: " + WeatherInfo.daily[i].wind_speed + " m/s";
+        var humidityDaily = "Humidity: " + WeatherInfo.daily[i].humidity + "%";
+
+        var card = "<div class='border col-12 col-md-2 col-lg-2 pt-2 pb-2'>" + dateDaily + "<br>" + iconDaily + "<br>" + tempDaily + "<br>" + windDaily + "<br>" + humidityDaily + "</div>"
+        forecastCardsEl.innerHTML += card;
+    }
 }
 
 citySearch.addEventListener("submit", formSubmitHandler);
